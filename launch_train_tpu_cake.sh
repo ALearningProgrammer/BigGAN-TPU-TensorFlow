5744a01dbd1143c7a3584368ee89d71f9679203e
ALearningProgrammer <46230529+ALearningProgrammer@users.noreply.github.com>
2021-02-15 11:45:52 -0800

    Create a new batchfile to train for cake dataset

---
+++ launch_train_tpu_cake.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+python3.6 main_tpu.py --use-tpu \
+	--train-input-path gs://birdaus/Train1/* \
+	--eval-input-path gs://birdaus/Validation* \
+	--model-dir gs://birdaus/model \
+	--result-dir ./results \
+	--batch-size 256  \
+	--ch 64 \
+	--self-attn-res 64 \
+	--g-lr 0.0001 \
+	--d-lr 0.0004 \
+	--verbosity INFO \
+	--train-examples 1281167 \
+	--eval-examples 50000 \
+	--tag sagan \
+	--tag run-$RANDOM \
+	$@ &
+	
